0e66819bb6da378513c12f128d020d384afd9349
Fabio S Medeiros <fabiosmedeiros@gmail.com>
2026-02-23 12:46:19 -0300

    Criando a classe de contexto de banco de dados AppDbContext. Alterando a classe Startup.cs, adicionando configurações do contexto de banco de dados e do identity. Incluindo a string de conexão com o banco de dados. Aplicando o modelo inicial do migrations.

--- src/services/CrmUno.Identity.API/Startup.cs
+++ src/services/CrmUno.Identity.API/Startup.cs
@@ -1,15 +1,11 @@
+using CrmUno.Identity.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CrmUno.Identity.API
 {
@@ -25,6 +21,14 @@ namespace CrmUno.Identity.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+            
             services.AddControllers();
         }
 
@@ -39,6 +43,8 @@ namespace CrmUno.Identity.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
