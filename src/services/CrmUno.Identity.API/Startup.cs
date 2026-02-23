263e3a8a08e452478c219d4c6d3c332b5875f4b3
Fabio S Medeiros <fabiosmedeiros@gmail.com>
2026-02-23 16:44:54 -0300

    Criando o controller IdentityController, com uma action para o registro de novas contas, e outra action para a autenticação de usuários. Criando view models para o registro e login de usuários. Adicionando a referência Swashbuckle no arquivo de projeto CrmUno.Identity.API.csproj, para instalação do Swagger. Alterando a classe Startup para inclusão/configuração do Swagger. Alterando o arquivo launchSettings.json, para definição do swagger na inicialização da aplicação.

--- src/services/CrmUno.Identity.API/Startup.cs
+++ src/services/CrmUno.Identity.API/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace CrmUno.Identity.API
 {
@@ -30,11 +32,29 @@ namespace CrmUno.Identity.API
                 .AddDefaultTokenProviders();
             
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "CRM Uno",
+                    Description = "App CRM Uno - A melhor alternativa para o seu negócio.",
+                    Contact = new OpenApiContact() { Name = "Fabio S Medeiros", Email = "fabiosmedeiros@gmail.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
